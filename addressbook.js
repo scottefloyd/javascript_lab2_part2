22030678c6230ca4de7e6466fbb654c366b46302
scott <scottefloyd@gmail.com>
2019-05-08 22:58:45 -0400

    cleaned things up and getting sleep

--- addressbook.js
+++ addressbook.js
@@ -1,7 +1,5 @@
 "use strict";
 
-window.onload = function() {
-
 class Contact {
   constructor(name, email, phone, relation) {
     this.name = name;
@@ -20,103 +18,59 @@ class AddressBook {
   }
 
   display() {
-    //const el = document.createElement("div");
     const itemUL = document.querySelector("section.display_contacts");
     itemUL.innerHTML = "";
+
+    let count = 0;
 
     for (let index in book.contacts) {
       const itemUL = document.querySelector("section.display_contacts");
       const el = document.createElement("div");
+      el.setAttribute("index", count);
+      el.classList.add("contact_box");
       el.innerHTML = `
       <p>Name: ${book.contacts[index].name}</p>
       <p>Email: ${book.contacts[index].email}</p>
       <p>Phone: ${book.contacts[index].phone}</p>
       <p>Relation: ${book.contacts[index].relation}</p>
-      <button i="${index}" class="add_btn">Delete</button>
-      `;
+      <span class="trash_can"><i class="material-icons trash_can delete-circle">delete-circle</i></span>`;
       itemUL.append(el);
+      count++
     }
 
   }
 
   addItem(info) {
-    this.contacts.push(info);
-  //  console.log(this.contacts);
-    book.display();
-
-    //const addItem = (info) => book.contacts = [ ...book.contacts, { ...info } ];
-
-   // console.log(addItem());
+    this.contacts = [...this.contacts, { ...info }];
     
+    document.querySelectorAll("input")[0].value = "";
+    document.querySelectorAll("input")[1].value = "";
+    document.querySelectorAll("input")[2].value = "";
+    document.querySelectorAll("input")[3].value = "";
+
+    book.display();
   }
 
-  deleteAt(index) {
-    this.contacts.splice(index, 1);
-  }
-  print() {
-    for (let i = 0; i < this.contacts.length; i++) {
-     // console.log(`Index: ${i}, Name: ${this.contacts[i].name}, Email: ${this.contacts[i].email}, Phone: ${this.contacts[i].phone}, Relation: ${this.contacts[i].relation}`);
-    }
+  deleteContact(index) {
+    this.contacts = [...this.contacts.slice(0, index), ...myContacts.slice(index + 1)];
   }
 }
-
-//const addItem = (newItem) => book.contacts = [ ...book.contacts, { ...newItem } ];
-
-
-
-
 
 
 const main = document.querySelector("main");
 
-main.addEventListener("click", function(event) {
+main.addEventListener("click", function (event) {
   if (event.target.classList.contains("add_btn")) {
 
-    let inputName = document.querySelector("#input_name").value;
-    let email = document.querySelector("#input_email").value;
-    let phone = document.querySelector("#input_phone").value;
-    let relation = document.querySelector("#input_relation").value;
+    let inputElements = document.querySelectorAll("input");
+    book.addItem(new Contact(inputElements[0].value, inputElements[1].value, inputElements[2].value, inputElements[3].value));
 
-    book.addItem(new Contact(inputName, email, phone, relation));
-
-    // display(itemList[event.target.attributes[0].value]);
-    //updateTotal();
   } else if (event.target.classList.contains("trash_can")) {
-    removeItem(event.target.attributes[0].value);
-    event.target.parentNode.remove();
-    //updateTotal();
+    event.target.parentNode.parentNode.remove();
   }
 });
 
-
 const book = new AddressBook();
 
-// while (true) {
-//   let choice = prompt("Add, Delete, Print, or Quit?");
-//   if (choice === "Quit") {
-//     console.log("Goodbye.");
-//     break;
-//   } else if (choice === "Print") {
-//     book.print();
-//   } else if (choice === "Delete") {
-//     let deleteChoice = prompt("Delete by index or by name?");
-//     if (deleteChoice === "Name") {
-//       book.deleteByName(prompt("Enter a name."));
-//     } else if (deleteChoice === "Index") {
-//       book.deleteAt(prompt("Index to delete?"));
-//     }
-//   } else if (choice === "Add") {
-//     book.add(new Contact(
-//       prompt("Enter a name."),
-//       prompt("Enter an email."),
-//       prompt("Enter a phone number."),
-//       prompt("Enter a relation.")
-//     ));
-//   }
-// }
 
 book.display();
-
-console.log(book.contacts);
-
-}
